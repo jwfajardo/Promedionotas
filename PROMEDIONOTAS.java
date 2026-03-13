2d2e6ade175bd45d97e54263f0dd2e615c9784b5
JOSE FAJARDO <jwfajardo22@gmail.com>
2026-03-12 23:22:18 -0500

    Primer commit

---
+++ PROMEDIONOTAS.java
@@ -0,0 +1,12 @@
+
+package com.mycompany.promedionotas;
+
+public class PROMEDIONOTAS {
+static double promedio(double a, double b,double c){
+    return (a + b + c) / 3;
+}
+    public static void main(String[] args) {
+         System.out.println("Promedio: " + promedio(8, 10,9));
+
+    }
+}
